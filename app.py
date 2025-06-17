4d31dbb37b3a0e097434ad8cf1491cee570a4d5f
modelifynetwork <modelifynetwork@gmail.com>
2025-06-17 15:01:49 -0300

    Update app.py

--- app.py
+++ app.py
@@ -747,6 +747,7 @@ def criar_pagamento_pix():
 
         # Buscar o produto NO BANCO, incluindo o email do usuário dono do produto
         conn = connect_db()
+        conn.row_factory = sqlite3.Row
         cursor = conn.cursor()
         cursor.execute(
             'SELECT nome, preco, uuid, usuario_email FROM produtos WHERE id = ?', (produto_id,)
